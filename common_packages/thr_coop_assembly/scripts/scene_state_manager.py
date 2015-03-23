4e42fa78c0b84029efa7ef40379bd327aff73041
Yoan Mollard <yoan.mollard@inria.fr>
2015-03-23 12:27:00 +0100

    Added a scene state manager + objects.yaml, missing predicate generation

---
+++ common_packages/thr_coop_assembly/scripts/scene_state_manager.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+from thr_coop_assembly.srv import GetSceneState, GetSceneStateResponse
+from thr_coop_assembly.msg import SceneState, Predicate
+
+class SceneStateManager(object):
+    def __init__(self):
+        self.state = SceneState()
+        try:
+            self.objects = rospy.get_param('/optitrack/objects')
+        except KeyError:
+            raise KeyError("Unable to read /optitrack/objects, have you used roslaunch?")
+        self.poses = {}
+        self.tfl = tf.TransformListener()
+
+    def generate_predicate(self):
+        self.state.header.stamp = rospy.Time.now()
+        raise NotImplementedError("scene_state_manager.generate_predicate() should update self.state.predicates "
+                                  "based on the self.poses map {'/human/wrist':[[x, y, z], [x, y, z, w]], ...}")
+
+    def update_state(self):
+        for obj in self.objects:
+            self.poses[obj] = self.tfl.lookupTransform("base", obj, rospy.Time(0))
+        self.generate_predicate()
+
+    def handle_request(self, req):
+        self.update_state()
+        resp = GetSceneStateResponse()
+        resp.commands = self.reader.get_user_commands()
+        return resp
+
+    def run(self):
+        s = rospy.Service('/thr/scene_state', GetSceneState, self.handle_request)
+        rospy.loginfo('[SceneStateManager] server ready to track {}...'.format(str(self.objects)))
+        rospy.spin()
+
+if __name__ == "__main__":
+    rospy.init_node('scene_state_manager')
+    SceneStateManager().run()
