008af1d2ff40085b02c91a4699f992cea15bf938
Yoan Mollard <yoan.mollard@inria.fr>
2015-04-09 18:07:23 +0200

    First implementation of perception + params scene to generalize

--- common_packages/thr_coop_assembly/scripts/scene_state_manager.py
+++ common_packages/thr_coop_assembly/scripts/scene_state_manager.py
@@ -1,45 +1,82 @@
 #!/usr/bin/env python
 
-import rospy
-import tf
+import rospy, rospkg, tf, transformations
 from thr_coop_assembly.srv import GetSceneState, GetSceneStateResponse
 from thr_coop_assembly.msg import SceneState, Predicate
 from itertools import combinations
+import json
 
 class SceneStateManager(object):
     def __init__(self):
         self.state = SceneState()
         self.world = "base"
+
         try:
-            self.objects = rospy.get_param('/optitrack/objects')
+            self.objects = rospy.get_param('/optitrack/objects')  # TODO get from something specific to the scene instead
         except KeyError:
             raise KeyError("Unable to read /optitrack/objects, have you used roslaunch?")
-        self.poses = {}
+        self.scene = rospy.get_param('/thr/scene')
+
+        self.rospack = rospkg.RosPack()
+        with open(self.rospack.get_path("thr_coop_assembly")+"/config/constraints.json") as f:
+            self.constraints = json.load(f)[self.scene]
+
+        with open(self.rospack.get_path("thr_coop_assembly")+"/config/perception.json") as f:
+            self.config = json.load(f)
+
+        self.predicates = []
         self.tfl = tf.TransformListener()
 
-    def generate_predicate(self):
+    def update_state(self):
+        """
+        This function updates the scene state by creating predicates observed from tf and
+        :return:
+        """
         self.state.header.stamp = rospy.Time.now()
 
         # Predicate Attached()
-        for o1, o2 in combinations(self.objects):
-            pass
+        for o1, o2 in combinations(self.objects, 2):
+            try:
+                human_relative = self.tfl.lookupTransform(o1, o2, rospy.Time(0))
+            except:
+                pass
+            else:
+                key = o1+'_'+o2 if o1<o2 else o2+'_'+o1
+                try:
+                    cart_dist = transformations.distance(self.constraints[key], human_relative)
+                except KeyError:
+                    pass
+                else:
+                    # TODO this measures only cartesian distance, missing quaternions
+                    if cart_dist<self.config['position_tolerance']:
+                        p = Predicate()
+                        p.type = Predicate.ATTACHED
+                        p.objects = [o1, o2]
+                        self.predicates.append(p)
 
-        # InRobotHand
-        for o in self.objects:
-            pass
+        # InHumanHand, InRobotHand
+        # TODO InRobotHand missing, need to check both arms
+        for object in self.objects:
+            try:
+                human_relative = self.tfl.lookupTransform('/human/wrist', object, rospy.Time(0))
+                #robot_relative = self.tfl.lookupTransform('', o2, rospy.Time(0))
+            except:
+                pass
+            else:
+                d = transformations.norm(human_relative)
+                if d<self.config['in_human_hand_tolerance']:
+                    p = Predicate()
+                    p.type = Predicate.HOLDBYHUMAN
+                    p.objects = [object]
+                    self.predicates.append(p)
 
-        raise NotImplementedError("scene_state_manager.generate_predicate() should update self.state.predicates "
-                                  "based on the self.poses map {'/human/wrist':[[x, y, z], [x, y, z, w]], ...}")
-
-    def update_state(self):
-        for obj in self.objects:
-            self.poses[obj] = self.tfl.lookupTransform(self.world, obj, rospy.Time(0))
-        self.generate_predicate()
 
     def handle_request(self, req):
+        self.predicates = []
         self.update_state()
         resp = GetSceneStateResponse()
-        resp.commands = self.reader.get_user_commands()
+        resp.state.header.stamp = rospy.Time.now()
+        resp.state.predicates = self.predicates
         return resp
 
     def run(self):
