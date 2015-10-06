203e162267a2cb0107ab9b0b40e185da6a2037db
Yoan Mollard <yoan.mollard@inria.fr>
2015-10-06 17:27:39 +0200

    Rotate the screen to look at objects

--- common_packages/thr_coop_assembly/scripts/display/concurrent_action_display.py
+++ common_packages/thr_coop_assembly/scripts/display/concurrent_action_display.py
@@ -7,13 +7,15 @@ import json, cv2, cv_bridge
 from numpy import zeros, uint8
 from sensor_msgs.msg import Image
 from collections import deque
+from baxter_commander import FaceCommander
 
 class ConcurrentActionDisplay(object):
-    def __init__(self, width, height, face=cv2.FONT_HERSHEY_SIMPLEX, scale=1, thickness=1, color=[255]*3, interline=1.1):
+    def __init__(self, width, height, font=cv2.FONT_HERSHEY_SIMPLEX, scale=1, thickness=1, color=[255]*3, interline=1.1):
+        self.face = FaceCommander()
         self.rospack = rospkg.RosPack()
         self.history_lock = Lock()
         self.action_history_name = '/thr/action_history'
-        self.face = face
+        self.font = font
         self.scale = scale
         self.thickness = thickness
         self.color = color
@@ -25,7 +27,6 @@ class ConcurrentActionDisplay(object):
 
         with open(self.rospack.get_path("thr_coop_assembly")+"/config/display.json") as f:
             self.text = json.load(f)
-
         rospy.Subscriber(self.action_history_name, ActionHistoryEvent, self.cb_action_event_received)
 
     def cb_action_event_received(self, msg):
@@ -37,25 +38,33 @@ class ConcurrentActionDisplay(object):
 
         with self.history_lock:
             try:
-                self.display_text(self.text[msg.action.type][map_const(msg.type)], msg.action.parameters,
-                                  self.face, self.scale, self.thickness, self.color, self.interline)
+                action = self.text[msg.action.type][map_const(msg.type)]
             except KeyError:
                 pass
+            else:
+                self.display_text(action['text'], msg.action.parameters,
+                                  self.font, self.scale, self.thickness, self.color, self.interline)
+                if isinstance(action['look_at'], int):
+                    if action['look_at'] >= 0:
+                        self.face.look_at(msg.action.parameters[action['look_at']])
+                    else:
+                        self.face.look_at(None)  # Reset gaze at neutral position
 
-    def display_text(self, lines, parameters=[], face=cv2.FONT_HERSHEY_SIMPLEX, scale=1, thickness=1, color=[255]*3, interline=1.1):
+
+    def display_text(self, lines, parameters=[], font=cv2.FONT_HERSHEY_SIMPLEX, scale=1, thickness=1, color=[255]*3, interline=1.1):
         def center(sentence):
-            (width, height), _ = cv2.getTextSize(sentence, face, scale, thickness)
+            (width, height), _ = cv2.getTextSize(sentence, font, scale, thickness)
             return (self.width-width)/2, height
 
 
-        y0 = (self.height - len(lines)*cv2.getTextSize('_', face, scale, thickness)[0][1]*interline)/2
+        y0 = (self.height - len(lines)*cv2.getTextSize('_', font, scale, thickness)[0][1]*interline)/2
         img = zeros((self.height, self.width, 3), uint8)
         for line_i, line in enumerate(lines):
             if isinstance(line, int):  # ints are expanded with their associated parameter
                 line = parameters[line].split('/')[-1].replace('_', ' ').upper()
             x = center(line)[0]
             y = y0 + int(center(line)[1]*(line_i+1)*interline)
-            cv2.putText(img, line, (x, y), face, scale, color, thickness=thickness)
+            cv2.putText(img, line, (x, y), font, scale, color, thickness=thickness)
 
         #cv2.imshow("Screen", img)
         #cv2.waitKey(1)
@@ -66,5 +75,5 @@ class ConcurrentActionDisplay(object):
 
 if __name__ == "__main__":
     rospy.init_node('concurrent_action_display')
-    ConcurrentActionDisplay(1024, 600, face=cv2.FONT_HERSHEY_SIMPLEX, scale=3, thickness=2, interline=2)
+    ConcurrentActionDisplay(1024, 600, font=cv2.FONT_HERSHEY_SIMPLEX, scale=3, thickness=2, interline=2)
     rospy.spin()
