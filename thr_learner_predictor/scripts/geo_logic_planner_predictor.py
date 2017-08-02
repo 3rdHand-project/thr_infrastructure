5ab7eaea0a5dfd244148662d72e1807f84f768d1
Baptiste Busch <baptiste.busch@gmail.com>
2017-08-02 18:28:39 +0200

    Add the predictor based on the geo_logic_planner

---
+++ thr_learner_predictor/scripts/geo_logic_planner_predictor.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+import rospy
+from thr_infrastructure_msgs.srv import StartStopEpisode, StartStopEpisodeRequest, StartStopEpisodeResponse
+from thr_infrastructure_msgs.srv import GetNextDecision, GetNextDecisionRequest, GetNextDecisionResponse
+from thr_infrastructure_msgs.srv import SetNewTrainingExample, SetNewTrainingExampleRequest, SetNewTrainingExampleResponse
+from thr_infrastructure_msgs.msg import Decision, Predicate
+
+from geo_logic_planner.srv import Plan
+from geo_logic_planner.msg import ShowSlide
+
+# To test this server, try: "rosservice call [/thr/learner or /thr/predictor] <TAB>" and complete the pre-filled request message before <ENTER>
+
+class Server(object):
+    def __init__(self):
+        self.sequence = 1  # ID of output actions
+        self.learner_name = '/thr/learner'
+        self.predictor_name = '/thr/predictor'
+        self.start_stop_service_name = '/thr/learner_predictor/start_stop'
+        self.current_action_idx = -1
+        rospy.Service(self.start_stop_service_name, StartStopEpisode, self.cb_start_stop)
+        rospy.wait_for_service('/geo_logic_planner/proxy_plan')
+        # get plan from planner
+        try:
+            proxy_plan = rospy.ServiceProxy('/geo_logic_planner/proxy_plan', Plan)
+            res = proxy_plan('no_reba', 'exp_baxter', True, 4, False)
+            self.planned_actions = res.logic_sequence
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+        self.slide_pub = rospy.Publisher('/geo_logic_planner/show_slide', ShowSlide, queue_size=10)
+        # initialize with the first action 
+        self.get_next_action()
+
+
+    def cb_start_stop(self, request):
+        if request.command == StartStopEpisodeRequest.START:
+            pass
+        elif request.command == StartStopEpisodeRequest.STOP:
+            pass
+        return StartStopEpisodeResponse()
+
+    def check_attached_pred(self, predictate_list, obj1, obj2, id_c=None):
+        return len([p for p in predictate_list if
+            p.type == 'attached' and obj1 in p.parameters and obj2 in p.parameters and
+            (id_c is None or str(id_c) in p.parameters)]) == 1
+
+    def check_positioned_pred(self, predictate_list, obj1, obj2, id_c=None):
+        return len([p for p in predictate_list if
+            p.type == 'positioned' and obj1 in p.parameters and obj2 in p.parameters and
+            (id_c is None or str(id_c) in p.parameters)]) == 1
+
+    def check_in_hws_pred(self, predictate_list, obj):
+        return len([p for p in predictate_list if
+            p.type == 'in_human_ws' and obj in p.parameters]) == 1
+
+    def check_picked_pred(self, predictate_list, obj):
+        return len([p for p in predictate_list if
+            p.type == 'picked' and obj in p.parameters]) == 1
+
+    def check_at_home_pred(self, predictate_list, arm):
+        return len([p for p in predictate_list if
+            p.type == 'at_home' and arm in p.parameters]) == 1
+
+    def check_busy_pred(self, predictate_list, arm):
+        return len([p for p in predictate_list if
+            p.type == 'busy' and arm in p.parameters]) == 1
+
+    def check_is_holding(self, predicate_list):
+        return len([p for p in predicate_list if p.type=="hold"])==1
+
+    def get_next_action(self):
+        self.current_action_idx += 1
+        self.slide_pub.publish(ShowSlide(self.current_action_idx))
+        literals = self.planned_actions[self.current_action_idx].literals
+        self.current_action = literals[0][9:]
+        self.current_action_param = literals[2:]
+
+    def predictor_handler(self, get_next_decision_req):
+        """
+        This handler is called when a request of prediction is received. It is based on a hardcoded policy
+        :param get_next_decision_req: an object of type GetNextDecisionRequest (scene state)
+        :return: an object of type GetNextDecisionResponse
+        """
+        decision_response = GetNextDecisionResponse()
+        obj_list = ['/toolbox/handle', '/toolbox/side_right', '/toolbox/side_left', '/toolbox/side_front', '/toolbox/side_back']
+        pred_list = get_next_decision_req.scene_state.predicates
+        in_hws_list = [o for o in obj_list if self.check_in_hws_pred(pred_list, o)]
+
+        decision = Decision()
+
+        if self.current_action == "grasping":
+            if not self.check_busy_pred(pred_list, "left"):
+                if not self.check_picked_pred(pred_list, self.current_action_param[0]):
+                    if not self.check_at_home_pred(pred_list, "left"):
+                        decision.type = 'start_go_home_left'
+                    else:
+                        decision.parameters = self.current_action_param
+                        decision.type = 'start_pick' 
+                else:
+                    self.get_next_action()
+                    decision.type = 'wait'
+            else:
+                decision.type = 'wait'
+
+        elif self.current_action == "placing":
+            if not self.check_busy_pred(pred_list, "left"):
+                if self.check_picked_pred(pred_list, self.current_action_param[0]):
+                    decision.parameters = self.current_action_param
+                    decision.type = 'start_place'
+                elif self.check_in_hws_pred(pred_list, self.current_action_param[0]):
+                    self.get_next_action()
+                    decision.type = 'wait'
+                else:
+                    if not self.check_at_home_pred(pred_list, "left"):
+                        decision.type = 'start_go_home_left'
+                    else:
+                        decision.parameters = [self.current_action_param[0]]
+                        decision.type = 'start_pick'
+            else:
+                decision.type = 'wait'
+
+        elif self.current_action == "handing":
+            if not self.check_busy_pred(pred_list, "left"):
+                if self.check_picked_pred(pred_list, self.current_action_param[0]):
+                    decision.parameters = self.current_action_param
+                    decision.type = 'start_give'
+                else:
+                    self.get_next_action()
+                    decision.type = 'wait'
+            else:
+                decision.type = 'wait'
+
+        elif self.current_action == "holding":
+            if not self.check_busy_pred(pred_list, "right"):
+                if self.check_positioned_pred(pred_list, self.current_action_param[1],
+                                              self.current_action_param[0],
+                                              self.current_action_param[2]):
+                    decision.parameters = [self.current_action_param[1], self.current_action_param[2]] 
+                    decision.type = 'start_hold'
+                    # this action do not terminate here so go to next action
+                    self.get_next_action()
+                else:
+                    decision.type = 'wait'
+            else:
+                decision.type = 'wait'
+
+        elif self.current_action == "screwing":
+            if self.check_attached_pred(pred_list, self.current_action_param[1],
+                                        self.current_action_param[0])
+                if not self.check_at_home_pred(pred_list, "right"):
+                    decision.type = 'start_go_home_right'
+                else:
+                    self.get_next_action()
+                    decision.type = 'wait'
+            else:
+                decision.type = 'wait'
